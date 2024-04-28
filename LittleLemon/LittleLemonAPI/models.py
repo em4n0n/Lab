e6c0b159007c6690a046d57a5ca7ffda7e7a1a44
em4n0n <58372945+em4n0n@users.noreply.github.com>
2024-04-28 15:07:32 -0700

    Update Models.py
    added Order and OrderItem classes

--- LittleLemon/LittleLemonAPI/models.py
+++ LittleLemon/LittleLemonAPI/models.py
@@ -31,4 +31,20 @@ class Cart(models.Model):
     
 class Order(models.Model):
     user = models.ForeignKey(User, ondelete=models.CASCADE)
-    
+    delivery_crew = models.ForeignKey(User, on_Delete=models.SET_NULL, related_name="delivery_crew", null=True)
+    status = models.BooleanField(db_index=True, default=0)
+    total = models.DecimalField(max_digits=6, decimal_places=2, default=0)
+    date = models.DateField(db_index=True)
+
+    def __str__(self):
+        return str(self.id)
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCASE)
+    menuitem = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
+    quantity = models.SmallIntegerField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+
+    class Meta:
+        unique_together = ('order', 'menuitem')
+
