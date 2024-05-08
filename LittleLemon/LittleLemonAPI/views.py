b774bdf50278ce675f8090da581e03378cb58722
em4n0n <58372945+em4n0n@users.noreply.github.com>
2024-05-07 20:57:20 -0700

    Update views.py

--- LittleLemon/LittleLemonAPI/views.py
+++ LittleLemon/LittleLemonAPI/views.py
@@ -6,3 +6,14 @@ from django.contrib.auth.models import User, Group
 from rest_framework.response import Response
 from decimal import Decimal
 from rest_framework.throttling import UserRateThrottle, AnonRateThrottle
+
+class MenuItemView(generics.ListAPIView, generics.ListCreateAPIView):
+    queryset = MenuItem.object.all()
+    serializer_class = MenuItemSerializer
+    ordering_fields = ['price']
+    search_fields = ['title']
+    throttle_classes = [AnonRateThrottle, UserRateThrottle]
+    def get_permissions(self):
+        if self.request.method == 'POST':
+            return [IsAdminUser()]
+        return [AllowAny()]
