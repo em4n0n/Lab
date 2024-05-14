9aece968b163e82d376bf94cffff93322760b71a
em4n0n <58372945+em4n0n@users.noreply.github.com>
2024-05-14 16:11:21 -0700

    Update urls.py
    orders and cart url

--- LittleLemon/LittleLemonAPI/urls.py
+++ LittleLemon/LittleLemonAPI/urls.py
@@ -8,8 +8,8 @@ url_patterns = [
     path('groups/manager/users/<int:pk', views.ManagerSingleUserView.as_view()),
     path('groups/delivery-crew/users', views.Delivery_Crew_Management.as_view()),
     path('groups/delivery-crew/users/<int:pk>/', views.Delivery_Crew_Management_Single_View.as_view()),
-    
-
-
+    path('cart/menu-items/', views.Customer_Cart.as_view()),
+    path('orders/', views.Orders_view.as_view()),
+    path('orders/<int:pk>/', views.Single_Order_View.as_view()),
 
 ]
