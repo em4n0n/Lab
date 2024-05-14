aa6650666ed94bdecd57e077b2e1273898f39011
em4n0n <58372945+em4n0n@users.noreply.github.com>
2024-05-13 18:46:48 -0700

    Update urls.py
    add manager url

--- LittleLemon/LittleLemonAPI/urls.py
+++ LittleLemon/LittleLemonAPI/urls.py
@@ -4,5 +4,8 @@ from . import views
 url_patterns = [
     path('menu-items/', views.MenuItemView.as_view()),
     path('menu-items/<int:pk>/', views.SingleItemView.as_view()),
-    
+    path('groups/manager/users/', views.ManagerUsersView.as_view()),
+    path('groups/manager/users/<int:pk', views.ManagerSingleUserView.as_view()),
+
+
 ]
