c0249495432548c39f201804282de7a2ecf737b7
em4n0n <58372945+em4n0n@users.noreply.github.com>
2024-05-15 22:31:27 -0700

    Update settings.py

--- LittleLemon/LittleLemon/settings.py
+++ LittleLemon/LittleLemon/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'LittleLemonAPI',
+    'LittleLemonAPI.apps.LittleLemonAPIConfig',
     'rest_framework',
     'djoser'
     'rest_framework.authtoken'
